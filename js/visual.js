7207b5148ca9a9c7e58d91976d48c65902eae6b6
linilinilyn~ <linilinilyn@gmail.com>
2023-05-30 18:08:06 +0900

    메인 슬라이드 페이지네이션 작업

--- js/visual.js
+++ js/visual.js
@@ -4,12 +4,12 @@
  * 기능 업데이트 : json데이터를 이용한 html 구조 생성 적용
  */
 
-window.addEventListener("load", function(event){
+window.addEventListener("load", function (event) {
     const swVisualWrap = document.querySelector(".sw-visual .swiper-wrapper");
     const xhr = new XMLHttpRequest();
-    xhr.addEventListener("readystatechange", function(event){
+    xhr.addEventListener("readystatechange", function (event) {
         let req = event.target;
-        if(req.readyState === XMLHttpRequest.DONE){
+        if (req.readyState === XMLHttpRequest.DONE) {
             let data = JSON.parse(req.response);
             // console.log(data);
             makeVisualHtml(data);
@@ -17,7 +17,8 @@ window.addEventListener("load", function(event){
     });
     xhr.open("GET", "data/visualdata.json");
     xhr.send();
-    function makeVisualHtml(_data){
+
+    function makeVisualHtml(_data) {
         let html = ``;
         _data.img.forEach((item, index) => {
             // console.log(item);
@@ -25,20 +26,24 @@ window.addEventListener("load", function(event){
                 <div class="swiper-slide" style="background-image: url(images/${item});"></div>
             `;
             html += temp;
-            
+
         });
         swVisualWrap.innerHTML = html;
 
         const swVisual = new Swiper(".sw-visual", {
-            loop : true,
-            effect : 'fade',
-            speed : 800,
+            loop: true,
+            effect: 'fade',
+            speed: 800,
             autoplay: {
                 delay: 3000,
                 disableOnInteraction: false,
             },
+            pagination: {
+                el: ".sw-visual-pg",
+                clickable: true,
+            },
         });
     }
-    
+
 
 })

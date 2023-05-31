28675ade2d1c406acb426e55da0c46b0d2061f52
linilinilyn~ <linilinilyn@gmail.com>
2023-05-31 18:10:59 +0900

    굿즈영역 작업중

--- js/visual.js
+++ js/visual.js
@@ -43,7 +43,46 @@ window.addEventListener("load", function (event) {
                 clickable: true,
             },
         });
+        // 위의 구문을 통해서 slide 완료되면 
+        // .sw-visual-pg 에는 span.swiper-pagination-bullet 이 생성됨.
+        // innerHtml 을 이용해서 내용을 넣어보자
+        const swVisualBullets = document.querySelectorAll(".sw-visual-pg .swiper-pagination-bullet");
+        swVisualBullets.forEach((item, index, arr) => {
+            // console.log(item);
+            // console.log(index);
+            // console.log(arr);
+
+            if(index < 9){
+                item.innerHTML = `<em>0${index + 1}</em>`;
+            } else {
+                item.innerHTML = `<em>${index + 1}</em>`;
+            }
+
+            // item.innerHTML = `<em>${index < 9 ? '0' : ''}${index + 1}</em>`;
+        });
     }
 
+    // 스크롤에 의한 position:fixed, relative 교체
+    const visual = this.document.querySelector(".visual");
+    window.addEventListener("scroll", function(){
+        // 스크롤 위치값을 파악
+        let scY = this.window.scrollY;
+        const header = this.document.querySelector(".header")
+        // console.log(scY);
 
-})
+        // classList.add()와 classList.remove()활용
+        // if(scY > 0){
+        //     // 스크롤바가 아래로 조금이라도 이동
+        //     // position : fixed;
+        //     header.classList.add("header-fixed");
+        //     visual.classList.add("visual-fixed");
+        // }else{
+        //     // 스크롤바가 최상단에 위치
+        //     // position : relative;
+        //     header.classList.remove("header-fixed");
+        //     visual.classList.remove("visual-fixed");
+        // }
+        
+
+    })
+});
